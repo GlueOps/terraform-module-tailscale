54f17e09722f70a73466451002265f947270290e
Venkat <venkata@venkatamutyala.com>
2025-07-14 19:22:41 -0700

    chore: fix comments

--- nameservers.tf
+++ nameservers.tf
@@ -5,7 +5,7 @@ resource "tailscale_dns_nameservers" "nameservers" {
     "9.9.9.9", #quad9
     "1.0.0.1", #cloudflare
     "8.8.4.4", #google
-    "149.112.112.112", #cloudflare
+    "149.112.112.112", #quad9
     "2606:4700:4700::1111", #cloudflare
     "2001:4860:4860::8888", #google
     "2620:fe::fe", #quad9
