31cf7ca9bdd2a6580660c15378e5b41f206e0a4c
Bazmahou <bouissihamza6@gmail.com>
2026-07-29 19:22:13 +0000

    fix: syntax error

--- acls.tf
+++ acls.tf
@@ -8,10 +8,10 @@ resource "tailscale_acl" "acl_config" {
 
     tagOwners = merge(
       {
-        "tag:sharedssh" = ["autogroup:admin"],
-        "tag:ci"        = ["autogroup:admin"],
-        "tag:exitnode"  = ["autogroup:admin"],
-        "tag:github-actions":    ["autogroup:admin"],
+        "tag:sharedssh"      = ["autogroup:admin"],
+        "tag:ci"             = ["autogroup:admin"],
+        "tag:exitnode"       = ["autogroup:admin"],
+        "tag:github-actions" = ["autogroup:admin"],
         "tag:app-nonprod-provisioner-nodes" : ["group:nonprod-cde-admins"],
         "tag:app-prod-provisioner-nodes" : ["group:prod-cde-admins"],
         "tag:captain-clusters" : ["group:captain-cluster-admins"]
@@ -92,16 +92,18 @@ resource "tailscale_acl" "acl_config" {
             dst = ["autogroup:internet"]
             ip  = ["*"]
             via = [local.cde_exit_node_tags[entity].prod]
-          },
-          
-        ],
-        {
-      			"dst": ["autogroup:internet"],
-      			"ip":  ["*"],
-      			"src": ["tag:github-actions"],
-      			"via": ["tag:app-prod-cde-exitnode-glueops"],
-      	},
+          }
+        ]
       ]),
+      # Grant GitHub Actions runners internet access via a CDE prod exit node
+      [
+        {
+          src = ["tag:github-actions"]
+          dst = ["autogroup:internet"]
+          ip  = ["*"]
+          via = [local.cde_exit_node_tags[var.github_actions_exit_node_entity].prod]
+        }
+      ],
       # Sysadmin SSH access to exit nodes for maintenance
       [
         {
